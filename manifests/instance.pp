376759bbe7a5efceb4756649c20dcf22df745293
Bryan Belanger <bbelanger@azcender.com>
2014-08-04 15:40:55 -0400

    Added test notify 6

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -392,8 +392,7 @@ define java_web_application_server::instance (
     "${instance_basedir}/${application_root}/webapps/${application_root}.war"
 
   # Notify the available applictaions
-  $groupid = "$available_applications[$application][group_id]"
-  notify { "$groupid": }
+  notice ( $available_applications[$application][group_id] )
 
   # Currently using an if statement since maven does not have an ensure
   # property. Need to address
