0bfd2d154f6b4e14e807f807f417ddad6df817d4
Bryan Belanger <bbelanger@azcender.com>
2014-08-04 18:37:48 -0400

    Test w/ out app isntall 5

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -39,7 +39,7 @@ define java_web_application_server::instance (
   $instance_basedir       = '/srv/tomcat',
   $application_root       = '') {
 
-  file { '/tmp/avail_app':
+  file { "/tmp/avail_app-${applicaton_root}"':
     content =>  'I am here',
   }
 
