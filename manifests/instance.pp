2f35ec9b30ee59f900fb279c663c6915069283b4
Bryan Belanger <bbelanger@azcender.com>
2014-08-04 19:35:54 -0400

    Adding concat

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -69,29 +69,29 @@ define java_web_application_server::instance (
     ])
 
   # Build a server.xml with resource context
-#  ::concat {'/tmp/x_server.xml':
-#    mode  => '0644',
-#    owner => vagrant,
-#    group => vagrant,
-#  }
-#
-#  ::concat::fragment {'server.xml_header':
-#    target  => '/tmp/x_server.xml',
-#    content => template('java_web_application_server/server_header.xml.erb'),
-#    order   => 01,
-#  }
-#
-#  ::concat::fragment {'server.xml_resources':
-#    target  => '/tmp/x_server.xml',
-#    content => template('java_web_application_server/server_context.xml.erb'),
-#    order   => 90,
-#  }
-#
-#  ::concat::fragment {'server.xml_footer':
-#    target  => '/tmp/x_server.xml',
-#    content => template('java_web_application_server/server_footer.xml.erb'),
-#    order   => 99,
-#  }
+  ::concat {'/tmp/x_server.xml':
+    mode  => '0644',
+    owner => vagrant,
+    group => vagrant,
+  }
+
+  ::concat::fragment {'server.xml_header':
+    target  => '/tmp/x_server.xml',
+    content => template('java_web_application_server/server_header.xml.erb'),
+    order   => 01,
+  }
+
+  ::concat::fragment {'server.xml_resources':
+    target  => '/tmp/x_server.xml',
+    content => template('java_web_application_server/server_context.xml.erb'),
+    order   => 90,
+  }
+
+  ::concat::fragment {'server.xml_footer':
+    target  => '/tmp/x_server.xml',
+    content => template('java_web_application_server/server_footer.xml.erb'),
+    order   => 99,
+  }
 
   ::tomcat::instance { $application_root:
     ensure           => $ensure,
