5b3874c6c416ac29acbb87b09288d8bb873f9bf8
Bryan Belanger <bbelanger@azcender.com>
2014-09-16 10:54:26 -0400

    Removed default instance_basedir

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -34,16 +34,13 @@ define java_web_application_server::instance (
   $ajp_port         = '8009',
   $server_port      = '8005',
   $ensure           = present,
-  $instance_basedir = '/srv/tomcat',
+  $instance_basedir,
   $resources        = []) {
 
   # This currently requires tomcat and maven classes
   require ::tomcat
 
   include ::java_web_application_server::params
-
-  # Create the instance directory based of application name
-  $instance_dir = "/srv/tomcat/${name}"
 
   # Validate application list and resource list are arryas
   validate_hash($applications)
@@ -65,9 +62,11 @@ define java_web_application_server::instance (
     'absent'
     ])
 
+  # Create the instance directory based of application name
+  $instance_dir = "${instance_basedir}/${name}"
+
   ::tomcat::instance { $name:
     catalina_base => $instance_dir,
-    source_url    => 'http://mirror.cogentco.com/pub/apache/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-7.0.55.tar.gz'
   }->
   ::tomcat::config::server { $name:
     catalina_base => $instance_dir,
