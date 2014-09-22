fe7fc3446682137d5ef44a086fd1f93e2174aa14
Bryan Belanger <bbelanger@azcender.com>
2014-09-22 09:33:11 -0400

    Test require

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -70,10 +70,12 @@ define java_web_application_server::instance (
   ::tomcat::instance { $name:
     catalina_base => $instance_dir,
     source_url    => $source_url,
-  }->
+  }
+
  ::tomcat::config::server { $name:
     catalina_base => $instance_dir,
     port          => $server_port,
+    require       => Tomcat::Instance["$name"],
   }->
   ::tomcat::config::server::connector { "${name}-http":
     catalina_base         => $instance_dir,
