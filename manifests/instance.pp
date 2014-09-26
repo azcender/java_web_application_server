5a2b9d1c7db4c5a90ba2d22f978897d00c203fb2
Paul Talbot <prolixalias@gmail.com>
2014-09-26 14:07:52 -0400

    removed server service since it broke everything

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -100,14 +100,11 @@ define java_web_application_server::instance (
     require       => ::Tomcat::Instance[$name],
   }
 
-  ::tomcat::config::server::service { $name:
-    catalina_base => $instance_dir,
-    require       => [::Tomcat::Config::Server[$name]],
-  } ->
   ::tomcat::service { "tomcat-${name}":
     use_init      => true,
     service_name  => "tomcat-${name}",
     catalina_base => $instance_dir,
+    require       => [::Tomcat::Config::Server[$name]],
   }
 
   # Remove example apps
