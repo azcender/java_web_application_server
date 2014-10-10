d3b56e9f4be3aad5b4427362a93456ba79d0ad8b
Bryan Belanger <bbelanger@azcender.com>
2014-10-09 22:01:03 -0400

    Can I get a break?

--- manifests/maven.pp
+++ manifests/maven.pp
@@ -59,7 +59,7 @@ define java_web_application_server::maven (
   $application_url =
     "${maven_repo}/${_group_id}/${artifactid}/${version}/${artifactid}-${version}.${packaging}"
 
-  ::wget::fetch { $applications_url:
+  ::wget::fetch { $application_url:
     destination => "${catalina_base}/webapps", 
     cache_dir   => '/var/cache/wget',
     cache_file  => "${artifactid}-${version}.${packaging}",
